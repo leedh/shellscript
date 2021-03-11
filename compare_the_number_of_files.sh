502b571b7344e92abd03512da759e14dcc4cda19
DONG HEE LEE <4c4448@gmail.com>
2021-03-11 21:39:33 +0900

    Update a file

--- compare_the_number_of_files.sh
+++ compare_the_number_of_files.sh
@@ -5,30 +5,63 @@ echo "===This script will compare the number of files between two directories ==
 echo ""
 echo ""
 echo ": for example, "
-echo ": Directory 1 : /media/das/dropbox/data/MPC/imaging/dicom_from_scanner/"
-echo ": Directory 2 : /media/das/backup/dicom_from_scanner/"
+echo ": Main      : /media/das/backup/dicom_from_scanner/"
+echo ": Reference : /media/das/dropbox/data/MPC/imaging/dicom_from_scanner/"
 echo ""
 echo ""
 
-read -p "Enter the path of one directory : " DIR1
+read -p "Enter the path of main directory : " DIR1
 #DIR1="/media/das/MPC100_dicom_backup/dicom_from_scanner/"
+if [ ${DIR1: -1} != / ]
+then
+       DIR1=$DIR1/
+fi       
 
-read -p "Enter the path of the other directory : " DIR2
+read -p "Enter the path of reference directory : " DIR2
 #DIR2="/media/cocoan_mri/dicom_from_scanner/MPC100/"
+if [ ${DIR2: -1} != / ]
+then
+	DIR2=$DIR2/
+fi
+
 
 # the number of files
 DIR1_N=(`ls -1 $DIR1 | wc -l`)
 DIR2_N=(`ls -1 $DIR2 | wc -l`)
 
-echo "The number of directories in DIR1 : " $DIR1_N
-echo "The number of directories in DIR2 : " $DIR2_N
+echo "The number of directories in Main      : " $DIR1_N
+echo "The number of directories in Reference : " $DIR2_N
+
+if [ $DIR1_N -ne $DIR2_N ]
+then
+	echo "The number of subdir is different!!"
+	while true
+	do
+		read -r -p "Do you want to continue? (y/n) " choice
+		case "$choice" in
+			n|N) exit 111;;
+			y|Y) break;;
+			*) echo "Response not valid";;
+		esac
+	done
+else
+	echo "The number of subdir is the same!!"
+fi
+
 
 readarray -t DIR1_SUBDIR < <(find $DIR1 -maxdepth 1 -type d -printf '%P\n')
-readarray -t DIR2_SUBDIR < <(find $DIR2 -maxdepth 1 -type d -printf '%P\n')
+#readarray -t DIR2_SUBDIR < <(find $DIR2 -maxdepth 1 -type d -printf '%P\n')
 
-#echo "dir1_subdir : " ${DIR1_SUBDIR[@]}
+echo "Will Compare directories below.."
+for dir in ${DIR1_SUBDIR[@]}
+do
+	echo "-> " $dir
+done
+
+#echo "Will Compare these directories : " ${DIR1_SUBDIR[@]}
 #echo "dir2_subdir : " ${DIR1_SUBDIR[@]}
 
+echo ""
 echo ""
 echo "Now.....comapring is starting........."
 echo ""
@@ -37,27 +70,22 @@ echo ""
 MISSING_DIR=()
 
 # compare base drictoriy
-if [ $DIR1_N -eq $DIR2_N ]
-then
-	# compare sub directories
-	for dir in ${DIR1_SUBDIR[@]}
-	do
+# compare sub directories
+for dir in ${DIR1_SUBDIR[@]}
+do
 
-		DIR1_fn=$(find $DIR1$dir -type f | wc -l)
-		DIR2_fn=$(find $DIR2$dir -type f | wc -l)
+	DIR1_fn=$(find $DIR1$dir -type f | wc -l)
+	DIR2_fn=$(find $DIR2$dir -type f | wc -l)
 
-		if [ $DIR1_fn -eq $DIR2_fn ]
-		then
-			echo "..." $dir " is done..."
-		else
-			echo "Two sub directories of " $dir " have different number of files."
-			MISSING_DIR+=($dir)
-		fi
+	if [ $DIR1_fn -eq $DIR2_fn ]
+	then
+		echo "..." $dir " is done..."
+	else
+		echo "Two sub directories of " $dir " have different number of files."
+		MISSING_DIR+=($dir)
+	fi
 
-	done
-else
-	echo "Two base directories have differernt number of sub directories."
-fi
+done
 
 echo ""
 echo ""
